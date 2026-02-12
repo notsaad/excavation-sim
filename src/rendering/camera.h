ee3bddbe3d00b768e8ec3568f2ece48d6f095590
notsaad <saadmazharr@gmail.com>
2026-02-12 14:05:43 -0500

    feat: member variables and priv function
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- src/rendering/camera.h
+++ src/rendering/camera.h
@@ -5,11 +5,13 @@
 class Camera {
 private:
   glm::vec3 position;
-  glm::vec3 up;
-  float yaw;   // mouse X movement
-  float pitch; // mouse Y movement
+  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+  // initial horizontal rotation (-90deg means camera pointing down the -Z axis)
+  float yaw = glm::radians(-90.0f);   // mouse X movement
+  // initial vertical tilt
+  float pitch = 0.0f; // mouse Y movement
   glm::vec3 front;
-  float speed;
+  float speed = 0.05f;
   
   glm::vec3 updateFront(float yaw, float pitch);
 
